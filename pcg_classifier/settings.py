1316909742c887560e7f5a90071d0f263924ea6c
HiraIrshad <hirairshad236@gmail.com>
2025-05-12 11:53:57 +0500

    Update settings.py

--- pcg_classifier/settings.py
+++ pcg_classifier/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-my@48ixp5f&br&a0)51($odjfqbexysjkyd(ahxv2qj_(8zuz!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'localhost']
+ALLOWED_HOSTS = ['pcg-classifier-a6akbmaufgdjethr.northeurope-01.azurewebsites.net', 'localhost']
 
 
 # Application definition

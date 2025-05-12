03b6b193fd7775cb837b5ca1d226e8cd97142020
HiraIrshad <hirairshad236@gmail.com>
2025-05-12 17:08:54 +0500

    Update settings.py

--- pcg_classifier/settings.py
+++ pcg_classifier/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-my@48ixp5f&br&a0)51($odjfqbexysjkyd(ahxv2qj_(8zuz!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pcg-classifier-a6akbmaufgdjethr.northeurope-01.azurewebsites.net', 'localhost']
 

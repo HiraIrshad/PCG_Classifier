45e565aaa02128b06ee9fc0a29cd4fc47430b21a
HiraIrshad <hirairshad236@gmail.com>
2025-05-12 21:47:12 +0500

    Update settings.py

--- pcg_classifier/settings.py
+++ pcg_classifier/settings.py
@@ -133,3 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Azure Storage Config
+AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=pcguploads;AccountKey=h7a7p5C6xT9ume/fKMpZKGcxZLRQviE1v5b0NBMs8SvurWHVvTN7+9FyJZL6tHw8WCYofrSSc/9F+AStLFvmhA==;EndpointSuffix=core.windows.net'
+AZURE_CONTAINER_NAME = 'pcg-uploads'
